6869f8019c5bdb52ec76e165853d93047ef70ea3
Darwin073100 <edwinegq@outlook.com>
2022-09-19 00:59:31 -0500

    Configuracion para procesar html y css con webpack

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import './main.css';
+
 const url = "http://localhost:8090/persona/all";
 const description = `Lorem ipsum dolor sit amet consectetur adipisicing elit. 
 Vel provident voluptatum, sed, quae enim inventore ipsum non unde`;
