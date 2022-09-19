0a8a8d39aa2dd8ffbed44b5f2672341898f78fdb
Darwin073100 <edwinegq@outlook.com>
2022-09-19 14:47:05 -0500

    Webpack Dev Server

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CopyPlugin = require('copy-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
-console.log('Hola');
+
 module.exports = {
   entry: "./src/index.js",
   output: {
@@ -54,4 +54,10 @@ module.exports = {
     }),
     new Dotenv(),
   ],
+  devServer:{
+    static: path.join(__dirname, 'dist'),
+    compress: true,
+    historyApiFallback: true,
+    port: 3006,
+  },
 };
